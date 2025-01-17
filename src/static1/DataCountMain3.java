1aeed9cf4b3ebd9efa9473b8d8319e309313de56
kimhaerinring <rlagofls0824@naver.com>
2025-01-17 10:48:55 +0900

    static 변수

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -3,10 +3,19 @@ package static1;
 public class DataCountMain3 {
     public static void main(String[] args) {
         Data3 data1 = new Data3("A");
-        System.out.println("A count = "+Data3.count);
+        System.out.println("A count = "+Data3.count); //data1.count가 아닌 Data3.count 클래스에 직접 접근
         Data3 data2 = new Data3("B");
         System.out.println("B count = "+Data3.count);
         Data3 data3  = new Data3("C");
         System.out.println("C count = "+Data3.count);
+
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4  = new Data3("C");
+        System.out.println(data4.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
     }
 }
